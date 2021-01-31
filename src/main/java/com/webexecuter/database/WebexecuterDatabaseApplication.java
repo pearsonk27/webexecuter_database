4502e2d4f1dbbac2d9da734bd9abf9713f93d293
Kristofer Pearson <pearsonk27@gmail.com>
2021-01-30 21:27:34 -0500

    Add starter liquibase project

---
+++ src/main/java/com/webexecuter/database/WebexecuterDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.webexecuter.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexecuterDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexecuterDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webexecuter/database/WebexecuterDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webexecuter.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebexecuterDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
